7f0d5742ed1de4ddadf1d980d557c172ccc6a610
jose <josecolomerbel@gmail.com>
2024-02-25 02:18:59 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let canvas_x = 1000;
+    let canvas_y = 1000;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
