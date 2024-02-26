4c332e14656703e414f85b957474291b4a3a4699
jose <josecolomerbel@gmail.com>
2024-02-26 22:12:33 +0100

    Add function to check if a node is inside a universe

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #[derive(Debug)]
-struct Node{
+struct Node {
     coor_x: u32,
     coor_y: u32,
     value: u32,
@@ -18,7 +18,7 @@ fn main() {
     println!("{:?}", test_var);
 }
 
-fn create_node(coor_x:u32, coor_y:u32, value:u32) -> Node {
+fn create_node(coor_x: u32, coor_y: u32, value: u32) -> Node {
     Node {
         coor_x: coor_x,
         coor_y: coor_y,
@@ -26,10 +26,18 @@ fn create_node(coor_x:u32, coor_y:u32, value:u32) -> Node {
     }
 }
 
-fn create_universe(size_x:u32, size_y:u32) -> Universe{
+fn create_universe(size_x: u32, size_y: u32) -> Universe {
     Universe {
         size_x: size_x,
         size_y: size_y,
+    }
+}
+
+fn is_node_in_universe(node: Node, universe: Universe) -> bool {
+    if node.coor_x <= universe.size_x && node.coor_y <= universe.size_y {
+        true
+    } else {
+        false
     }
 }
 
@@ -45,5 +53,19 @@ mod tests {
     fn point_exists() {
         let test_node = crate::create_node(40, 103, 0);
         assert_eq!(test_node.coor_x, 40);
+    }
+
+    #[test]
+    fn is_point_in_universe() {
+        let test_universe = crate::create_universe(100, 200);
+        let test_node = crate::create_node(40, 103, 0);
+        assert!(crate::is_node_in_universe(test_node, test_universe))
+    }
+
+    #[test]
+    fn is_point_outside_universe() {
+        let test_universe = crate::create_universe(100, 200);
+        let test_node = crate::create_node(40, 203, 0);
+        assert_eq!(crate::is_node_in_universe(test_node, test_universe), false)
     }
 }
