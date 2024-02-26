080eada2dd312a2f5024a34f4401c74060191742
jose <josecolomerbel@gmail.com>
2024-02-26 22:03:42 +0100

    Add create universe functions to delimit working area

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,49 @@
 #[derive(Debug)]
-struct Node {
+struct Node{
     coor_x: u32,
     coor_y: u32,
     value: u32,
 }
 
+#[derive(Debug)]
+struct Universe {
+    size_x: u32,
+    size_y: u32,
+}
+
 fn main() {
     // let canvas_x = 1000;
     // let canvas_y = 1000;
-    let test_var = create_random_node();
+    let test_var = create_node(40, 103, 0);
     println!("{:?}", test_var);
 }
 
-fn create_random_node() -> Node {
+fn create_node(coor_x:u32, coor_y:u32, value:u32) -> Node {
     Node {
-        coor_x: 40,
-        coor_y: 103,
-        value: 0,
+        coor_x: coor_x,
+        coor_y: coor_y,
+        value: value,
+    }
+}
+
+fn create_universe(size_x:u32, size_y:u32) -> Universe{
+    Universe {
+        size_x: size_x,
+        size_y: size_y,
     }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn point_exist() {
-        let test_node = crate::create_random_node();
+    fn universe_exists() {
+        let test_universe = crate::create_universe(100, 200);
+        assert_eq!(test_universe.size_x, 100);
+    }
+
+    #[test]
+    fn point_exists() {
+        let test_node = crate::create_node(40, 103, 0);
         assert_eq!(test_node.coor_x, 40);
     }
 }
