08bc84273def9b2f042fa5ce39c3a12ea9391bfb
jose <josecolomerbel@gmail.com>
2024-02-26 21:57:39 +0100

    Implement create nodes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
+#[derive(Debug)]
+struct Node {
+    coor_x: u32,
+    coor_y: u32,
+    value: u32,
+}
+
 fn main() {
-    let canvas_x = 1000;
-    let canvas_y = 1000;
+    // let canvas_x = 1000;
+    // let canvas_y = 1000;
+    let test_var = create_random_node();
+    println!("{:?}", test_var);
+}
+
+fn create_random_node() -> Node {
+    Node {
+        coor_x: 40,
+        coor_y: 103,
+        value: 0,
+    }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn point_exist() {
+        let test_node = crate::create_random_node();
+        assert_eq!(test_node.coor_x, 40);
     }
 }
